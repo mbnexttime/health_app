e9d217f1e0f1d40e4f109d5d847b7c290433b593
AndreyYurko <andreyyurko01@gmail.com>
2022-04-15 16:57:11 +0300

    complex reaction test complete

--- app/src/main/java/com/example/psyhealthapp/debug/DebugFragment.kt
+++ app/src/main/java/com/example/psyhealthapp/debug/DebugFragment.kt
@@ -16,7 +16,7 @@ class DebugFragment : Fragment(R.layout.debug_fragment) {
      */
     private val fragments: ArrayList<FragmentHolder> = arrayListOf(
         FragmentHolder {
-            return@FragmentHolder TestReactionFragment()
+            return@FragmentHolder ComplexTestReactionFragment()
         },
     )
 
